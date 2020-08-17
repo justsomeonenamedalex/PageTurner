3eba38dbf166ddced6af300fad2c60d936d6954f
Alex Davis <8m3xlt@gmail.com>
2020-08-17 14:00:44 +0100

    added moon phase command

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,9 @@
 import discord
 from discord.ext import commands
+import math
+import requests
+import json
+import time
 
 
 class Fun(commands.Cog):
@@ -11,6 +15,25 @@ class Fun(commands.Cog):
     async def pog(self, ctx):
         """Poggers!"""
         await ctx.send("poggers")
+
+    @commands.command(aliases=["moonphase", "moon_phase"])
+    async def moon(self, ctx):
+        """Gets current data on the moon"""
+        # This api is the most random thing i've found for this bot, so I wouldn't be surprised if it goes down
+        # TODO: read up on how the api works and recreate it in python
+        # https://github.com/FarmSense/Astro-Widget/blob/master/astro_widget.js
+        date = math.floor(time.time())
+        url = f"http://api.farmsense.net/v1/moonphases/?d={date}&callback=window.randName.f.moonPhase.parseRequest"
+        page = requests.get(url)
+        # TODO: do this with regex, even if it hurts my soul to do so
+        page_split_one = page.text.split("(")[1]
+        page_split_two = page_split_one.split(")")[0]
+        page = page_split_two[1:-1]
+        if page:
+            data = json.loads(page)
+            await ctx.send(f"The current phase of the moon is `{data['Phase']}`")
+        else:
+            await ctx.send("Page could not be found")
 
 
 def setup(client):
