8c899911d565f9b123a43a16297d5040bce448d4
Alex Davis <8m3xlt@gmail.com>
2020-08-17 13:49:01 +0100

    added fun.py

---
+++ cogs/fun.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+
+class Fun(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.config = client.config
+
+    @commands.command(aliases=["poggers"])
+    async def pog(self, ctx):
+        await ctx.send("poggers")
+
+
+def setup(client):
+    client.add_cog(Fun(client))
