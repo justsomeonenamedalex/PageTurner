fc69d04eeb1a326f40173418276b1d86068bc893
Alex Davis <8m3xlt@gmail.com>
2020-08-18 22:47:41 +0000

    Added description and fixed bugs in verify command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -9,14 +9,14 @@ class Moderation(commands.Cog):
 
     @commands.command()
     async def verify(self, ctx: commands.Context, member: discord.Member):
+        """Verifies the member and sends a welcome message"""
         unverified_role = ctx.guild.get_role(self.config["unverified_role"])
         member_role = ctx.guild.get_role(self.config["member_role"])
         await member.remove_roles(unverified_role)
         await member.add_roles(member_role)
 
         welcome_channel = ctx.guild.get_channel(self.config["welcome_channel"])
-        message = f"Welcome {member.mention} to the server.\nMake sure to get some roles from <#743068757190115399> , " \ 
-                  f"<#744588765888118835> and <#743089463038836778> and introduce yourself in <#743088854004662342> ! "
+        message = f"Welcome {member.mention} to the server.\nMake sure to get some roles from <#743068757190115399> , <#744588765888118835> and <#743089463038836778> and introduce yourself in <#743088854004662342> ! "
 
         await welcome_channel.send(message)
 
