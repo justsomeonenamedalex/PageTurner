ee2b4695b9107c645f4990f6933d09146331c897
Alex Davis <8m3xlt@gmail.com>
2020-09-05 20:15:05 +0100

    Proper eval command

--- cogs/dev.py
+++ cogs/dev.py
@@ -1,11 +1,27 @@
 import discord
 from discord.ext import commands
+import ast
 
 
 class Dev(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.config = client.config
+
+    def insert_returns(self, body):
+        # insert return stmt if the last expression is a expression statement
+        if isinstance(body[-1], ast.Expr):
+            body[-1] = ast.Return(body[-1].value)
+            ast.fix_missing_locations(body[-1])
+
+        # for if statements, we insert returns into the body and the orelse
+        if isinstance(body[-1], ast.If):
+            self.insert_returns(body[-1].body)
+            self.insert_returns(body[-1].orelse)
+
+        # for with blocks, again we insert returns into the body
+        if isinstance(body[-1], ast.With):
+            self.insert_returns(body[-1].body)
 
     @commands.is_owner()
     @commands.command(hidden=True)
@@ -129,6 +145,62 @@ class Dev(commands.Cog):
         embed = discord.Embed(title=f"Server icon for {ctx.guild.name}")
         embed.set_image(url=ctx.guild.icon_url_as(format="png"))
         await ctx.send(embed=embed)
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def run(self, ctx, *, cmd):
+        """Evaluates input.
+
+        Input is interpreted as newline seperated statements.
+        If the last statement is an expression, that is the return value.
+
+        Usable globals:
+          - `bot`: the bot instance
+          - `discord`: the discord module
+          - `commands`: the discord.ext.commands module
+          - `ctx`: the invokation context
+          - `__import__`: the builtin `__import__` function
+
+        Such that `>eval 1 + 1` gives `2` as the result.
+
+        The following invokation will cause the bot to send the text '9'
+        to the channel of invokation and return '3' as the result of evaluating
+
+        >eval
+        a = 1 + 2
+        b = a * 2
+        await ctx.send(a + b)
+        a
+        """
+        fn_name = "_eval_expr"
+
+        cmd = cmd.strip("` ")
+
+        # add a layer of indentation
+        cmd = "\n".join(f"    {i}" for i in cmd.splitlines())
+
+        # wrap in async def body
+        body = f"async def {fn_name}():\n{cmd}"
+
+        parsed = ast.parse(body)
+        body = parsed.body[0].body
+
+        self.insert_returns(body)
+
+        env = {
+            'bot': ctx.bot,
+            'discord': discord,
+            'commands': commands,
+            'ctx': ctx,
+            '__import__': __import__
+        }
+        exec(compile(parsed, filename="<ast>", mode="exec"), env)
+
+        try:
+            result = (await eval(f"{fn_name}()", env))
+            await ctx.send(f"```\n{result}\n```")
+        except Exception as e:
+            await ctx.send(f"```\nException:\n{e}\n```")
 
 
 def setup(client):
